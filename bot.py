b4a0c3c76d9914c0c88ec8c5875b6b65514c67d8
praneeth-coder-2005 <savediskofc@gmail.com>
2024-07-23 15:24:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import logging
 import subprocess
 
 # Replace with your actual bot token
-TOKEN = '6576942975:AAFISid4xagXKh8bO0INDeSdS0BHk4ljSkU'
+TOKEN = os.environ.get('6576942975:AAFISid4xagXKh8bO0INDeSdS0BHk4ljSkU')
 DEFAULT_WATERMARK_PATH = 'default_watermark.png'
 
 # Set up logging
